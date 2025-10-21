b570a57376e00e37ebca2168afa0387602b9249a
João_Santos <164881489+jptrava@users.noreply.github.com>
2025-10-21 15:05:10 -0300

    Delete api turmas atualizado directory

--- api turmas atualizado/src/view/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-// Definindo a porta para a aplicação
-const PORT = process.env.PORT || 3000;
-
-// Iniciando o servidor
-app.listen(PORT, () => {
-  console.log(`Servidor rodando em http://localhost:${PORT}`);
-});
